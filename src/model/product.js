f40a178c80b965f730d6d468015e2379c4be5610
João Amador <nnettoolliveira@gmail.com>
2022-07-12 02:26:58 -0300

    :tada: Base do projeto

---
+++ src/model/product.js
@@ -0,0 +1,10 @@
+export default class Product {
+  constructor(dedscription, buyPrice, sellPrice, stock) {
+    this.dedscription = dedscription
+    this.buyPrice = buyPrice
+    this.sellPrice = sellPrice
+    this.stock = stock
+  }
+}
+
+//module.exports = Product
