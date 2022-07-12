8842e6bd4ce5de32dd031217313fa02692baeb2b
João Amador <nnettoolliveira@gmail.com>
2022-07-12 03:16:04 -0300

    :bug: Corrigindo bug de baixa de estoque

--- test/product.test.js
+++ test/product.test.js
@@ -1,28 +1,30 @@
 import Product from '../src/model/product'
 import sell from '../src/service/sellProduct'
 
-test('deve validar baixa de estoque da venda de uma unidade', () => {
-  let produto = new Product(
-    'Celular',
-    500.00,
-    900.00,
-    10
-  )
+describe('Validações de produto', () => {
+  test('deve validar baixa de estoque da venda de uma unidade', () => {
+    let produto = new Product(
+      'Celular',
+      500.00,
+      900.00,
+      10
+    )
 
-  sell(produto, 1)
+    sell(produto, 1)
 
-  expect(produto.stock).toBe(9)
-})
+    expect(produto.stock).toBe(9)
+  })
 
-test('deve aceitar a venda de mais de uma unidade', () => {
-  let produto = new Product(
-    'Celular',
-    500.00,
-    900.00,
-    10
-  )
+  test('deve aceitar a venda de mais de uma unidade', () => {
+    let produto = new Product(
+      'Celular',
+      500.00,
+      900.00,
+      10
+    )
 
-  sell(produto, 3)
+    sell(produto, 3)
 
-  expect(produto.stock).toBe(7)
+    expect(produto.stock).toBe(7)
+  })
 })
